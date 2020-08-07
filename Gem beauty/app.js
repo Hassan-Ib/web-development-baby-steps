0f99a5042fe7e545d205ee5ca5db627a87d6fbb5
Ibrahim Hassan <azanebrahim@gmail.com>
2020-08-07 10:52:51 -0400

    modified:   Gem beauty/app.js 	deleted:    Gem beauty/images/realbeauty1.jpg 	modified:   Gem beauty/index.html 	modified:   Gem beauty/style.css 	modified:   menu/main.css

---
+++ Gem beauty/app.js
@@ -0,0 +1,6 @@
+const heroMsg = [
+    'kam lurem ',
+    'let us at <strong>gem beauty</strong> make the plan to give you the skin you deserve',
+    /*Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, saepe. Omnis sapiente sed
+    , laudantium ratione eum exercitationem sunt ipsum quas, delectus quisquam similique, mollitia dicta autem soluta laboriosam. Repellat, veritatis?*/
+];
